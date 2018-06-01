23de38ea33a3412d2f4f76f490d21ba530b5cbd3
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-06-01 14:37:24 -0300

    Clase evaluacion terminada

--- src/algoritmoCalidad/Evaluacion.java
+++ src/algoritmoCalidad/Evaluacion.java
@@ -2,14 +2,19 @@ package algoritmoCalidad;
 
 public class Evaluacion {
 
+	//Puntajes
 	public static final int BUENA = 2;
 	public static final int REGULAR = 1;
 	public static final int MALA = 0;
+	private double pesos[] = {1.5, 1, 1.5, 1, 1, 1};
 	
-	private boolean satisfaccion;
-	private boolean[] caracsEn0;
-	private boolean[] subCaracsEn0;
-	
+	//Resultados
+	private boolean satisfaccion = true;
+	private double caracs[];
+	private int subcaracs[];
+	private double calidad;
+
+	//Indices de caracteristicas
 	public static final int FUNCIONALIDAD = 0;
 	public static final int EFICIENCIA = 1;
 	public static final int FIABILIDAD = 2;
@@ -17,18 +22,31 @@ public class Evaluacion {
 	public static final int USABILIDAD = 4;
 	public static final int PORTABILIDAD = 5;
 	
-	public Evaluacion(int segAcceso, int exactitudRes, //Funcionalidad
-			int utilRecursos, int compEnTiempo, //Eficiencia
-			int toleranciaFallos, int capacidadRecuperacionErr, //Fiabilidad
-			int analizarCod, int cambiarCod, int estabilidad, //Mantenibilidad
-			int serEntendido, int serOperado, //Usabilidad
+	//Indices de subcaracteristicas
+	public static final int EXACTITUD_RESULTADOS = 0;
+	public static final int TOLERANCIA_FALLOS = 1;
+	
+	/**
+	 * Evaluacion se crea especificando los valores de cada subcaracteristica. Para ello,
+	 * puntuarlas con las constantes BUENA, MALA o REGULAR, segun corresponda.
+	 */
+	public Evaluacion(int seguridadDeAcceso, int exactitudDeResultados, //Funcionalidad
+			int utiluzacionDeRecursos, int comportamientoEnElTiempo, //Eficiencia
+			int toleranciaAFallos, int capacidadDeRecuperacionDeErrores, //Fiabilidad
+			int facilidadDeAnalizarCodigo, int facilidadDeCambiarCodigo, int estabilidad, //Mantenibilidad
+			int capacidadDeSerEntendido, int capacidadDeSerOperado, //Usabilidad
 			int adaptabilidad, int instalabilidad) //Portabilidad
 	{
-		calcular(segAcceso, exactitudRes, utilRecursos, compEnTiempo, toleranciaFallos, 
-				capacidadRecuperacionErr, analizarCod, cambiarCod, estabilidad, serEntendido, 
-				serOperado, adaptabilidad, instalabilidad);
+		calcular(seguridadDeAcceso, exactitudDeResultados, utiluzacionDeRecursos, comportamientoEnElTiempo,
+				toleranciaAFallos, capacidadDeRecuperacionDeErrores, facilidadDeAnalizarCodigo,
+				facilidadDeCambiarCodigo, estabilidad, capacidadDeSerEntendido, capacidadDeSerOperado, adaptabilidad,
+				instalabilidad);
 	}
 
+	/**
+	 * Calcula todos los resultados necesarios de la evaluacion. Recibe por parametro los 
+	 * valores de todas las subcaracteristicas.
+	 */
 	private void calcular(int segAcceso, int exactitudRes, //Funcionalidad
 			int utilRecursos, int compEnTiempo, //Eficiencia
 			int toleranciaFallos, int capacidadRecuperacionErr, //Fiabilidad
@@ -36,23 +54,103 @@ public class Evaluacion {
 			int serEntendido, int serOperado, //Usabilidad
 			int adaptabilidad, int instalabilidad) //Portabilidad 
 	{
-		int funcionalidad, eficiencia, fiabilidad, mantenibilidad, usabilidad, portabilidad; //Caracteristicas
+		caracs = new double[6];
+		subcaracs = new int[2];
 		
-		//Calculo las caracteristicas a partir de los promedios
-		funcionalidad = (segAcceso + exactitudRes)/2;
-		eficiencia = (utilRecursos + compEnTiempo)/2;
-		fiabilidad = (toleranciaFallos + capacidadRecuperacionErr)/2;
-		mantenibilidad = (analizarCod + cambiarCod + estabilidad)/3;
-		usabilidad = (serEntendido + serOperado)/2;
-		portabilidad = (adaptabilidad + instalabilidad)/2;
+		subcaracs[EXACTITUD_RESULTADOS] = exactitudRes;
+		subcaracs[TOLERANCIA_FALLOS] = toleranciaFallos;
+		
+		inicializarCaracs(segAcceso, exactitudRes, utilRecursos, compEnTiempo, toleranciaFallos,
+				capacidadRecuperacionErr, analizarCod, cambiarCod, estabilidad, serEntendido, serOperado, adaptabilidad,
+				instalabilidad);
+		calidad = calcularCalidad();
 		
 		//Filtros
-		if(funcionalidad == 0 || eficiencia == 0  || fiabilidad == 0 || mantenibilidad == 0 ||
-				usabilidad == 0 || portabilidad == 0)
-		{
-			//|| exactitudRes == MALA || toleranciaFallos == MALA){
-			//}
+		//Si alguna caracteristica esta en 0 o 
+		//exactitud de resultados o tolerancia a fallos son MALA o
+		//la calidad es menor al 50%: no puede ser satisfactorio
+		if(chequearCaracs() || exactitudRes == MALA || toleranciaFallos == MALA || calidad < 0.5)
 			satisfaccion = false;
+	}
+
+	/**
+	 * Pondera los valores de las caracteristicas y calcula el porcentaje de calidad.
+	 * @return porcentaje de calidad (numero entre 0 y 1)
+	 */
+	private double calcularCalidad() {
+		//Pondero el resultado
+		double numerador = 0;
+		double denominador = 0;
+		for(int i=0; i<6; i++) {
+			numerador += caracs[i] * pesos[i];
+			denominador += pesos[i];
 		}
+		double ponderacion = numerador/denominador;
+		
+		//Calculo el porcentaje
+		return ponderacion / 2; //Ponderacion puede valer entre 0 y 2, segun las constantes BUENA, REGULAR y MALA
+	}
+
+	/**
+	 * Chequea si hay alguna caracteristica en 0 y 
+	 */
+	private boolean chequearCaracs() {
+		//Si alguna caracteristica esta en 0, no puede ser satisfactorio
+		for(double carac : caracs)
+			if(carac == 0)
+				return true;
+		return false;
+	}
+
+	/**
+	 * Calcula los valores de cada caracteristica en base al promedio de sus subcaracteristicas
+	 */
+	private void inicializarCaracs(int segAcceso, int exactitudRes, int utilRecursos, int compEnTiempo,
+			int toleranciaFallos, int capacidadRecuperacionErr, int analizarCod, int cambiarCod, int estabilidad,
+			int serEntendido, int serOperado, int adaptabilidad, int instalabilidad) {
+		
+		//Calculo las caracteristicas a partir de los promedios
+		caracs[FUNCIONALIDAD] = (segAcceso + exactitudRes)/2;
+		caracs[EFICIENCIA] = (utilRecursos + compEnTiempo)/2;
+		caracs[FIABILIDAD] = (toleranciaFallos + capacidadRecuperacionErr)/2;
+		caracs[MANTENIBILIDAD] = (analizarCod + cambiarCod + estabilidad)/3;
+		caracs[USABILIDAD] = (serEntendido + serOperado)/2;
+		caracs[PORTABILIDAD] = (adaptabilidad + instalabilidad)/2;
+	}
+	
+	public boolean esSatisfactorio() {
+		return satisfaccion;
+	}
+	
+	public double getCalidad() {
+		return calidad;
+	}
+	
+	/**
+	 * Arma un array booleano que se accede con las constantes de las caracteristicas como indices
+	 * y por cada una dice si está en 0 o no.
+	 * @return
+	 */
+	public boolean[] getCaracsEn0() {
+		boolean[] resultado = new boolean[6];
+		for(int i=0; i<6; i++)
+			if(caracs[i] == 0)
+				resultado[i]=true;
+			else resultado[i]=false;
+		return resultado;
+	}
+	
+	/**
+	 * Arma un array booleano que se accede con las constantes de las subcaracteristicas como indices
+	 * y por cada una dice si está en 0 o no.
+	 * @return
+	 */
+	public boolean[] getSubcaracsEn0() {
+		boolean[] resultado = new boolean[2];
+		for(int i=0; i<2; i++)
+			if(subcaracs[i] == 0)
+				resultado[i]=true;
+			else resultado[i]=false;
+		return resultado;
 	}
 }
