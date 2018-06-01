845615377dce2b2d8dd27abeea8145546c86db2b
Valeria Silvestri <valeria.ayelen@gmail.com>
2018-06-01 12:54:43 -0300

    Clase evaluacion a medias

---
+++ src/algoritmoCalidad/Evaluacion.java
@@ -0,0 +1,14 @@
+package algoritmoCalidad;
+
+public class Evaluacion {
+
+	public static final int BUENA = 2;
+	public static final int REGULAR = 1;
+	public static final int MALA = 0;
+	
+	//Subcaracteristicas
+	private int segAcceso;
+	private int exactitudRes;
+	private int utilRecursos;
+	private int compEnTiempo;
+}
